3f3e03c61da4eae7fa0c3b31a1197132beaecad6
Aaron Thibodeau <aathibodeau@gmail.com>
2017-03-23 18:55:22 -0500

    Adding sorting ability

--- src/MainController.java
+++ src/MainController.java
@@ -1,22 +1,46 @@
 
+import java.io.IOException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-
-import java.io.IOException;
-
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class MainController extends main {
 	
 	@FXML
-	private Button Tester;
+	private Button Tester, btnSort;
 	@FXML
 	private ComboBox selectAlgo, Lang;
-	private String algo;
+	@FXML
+	private Label lab0, lab1, lab2, lab3, lab4, lab5, lab6, lab7, lab8, lab9, lab10, lab11, lab12, lab13, lab14;
 	private String lang;
-	
+	private Sorter sort = new Sorter();
+	private int[] array = {2,34,53,23,122,5,0,6,32,43,22,46,1000,3,452};
+
+	public void initialize() {
+		Label[] texts = {lab0, lab1, lab2, lab3, lab4, lab5, lab6, lab7, lab8, lab9, lab10, lab11, lab12, lab13, lab14 };
+		for(int i = 0; i < texts.length; i++) {
+			texts[i].setText(Integer.toString(array[i]));
+		}
+		selectAlgo.setValue("Select Algorithm");
+		
+		ObservableList<String> options = 
+			    FXCollections.observableArrayList(
+			        "Bubble",
+			        "Heap",
+			        "Merge",
+			        "Quick",
+			        "Insertion"
+			    );
+			 selectAlgo.getItems().addAll(options);
+	}
 	public void toTestScreen() {
    	    {
    	        try {
@@ -32,27 +56,7 @@ public class MainController extends main {
    	    }
 	}
 	
-	public void SelectAlgo() {
-		switch(selectAlgo.getValue().toString()) {
-		case "Merge" :
-			algo = "merge";
-			break;
-		case "Bubble" :
-			algo = "bubble";
-			break;
-		case "Quick" :
-			algo = "quick";
-			break;
-		case "Heap":
-			algo = "heap";
-			break;
-		case "Insertion" :
-			algo = "insertion";
-			break;
-		}
-	}
-	
-	public void SelectLang() {
+	public void selectLang() {
 		switch(Lang.getValue().toString()) {
 		case "Java" :
 			lang = "java";
@@ -71,4 +75,33 @@ public class MainController extends main {
 			break;
 		}
 	}
+	
+	public void sort() {
+			switch(selectAlgo.getValue().toString()) {
+			case "Merge" :
+				sort.startMerge(array);
+				break;
+			case "Bubble" :
+				sort.bubbleSort(array);
+				break;
+			case "Quick" :
+				sort.qSort(array);
+				break;
+			case "Heap":
+				sort.heapSort(array);
+				break;
+			case "Insertion" :
+				sort.insertion(array);
+				break;
+			case "Select Algorithm":
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("Error");
+				alert.setHeaderText(null);
+				alert.setContentText("Please select an algorithm");
+				alert.showAndWait();
+				break;
+			default:
+				System.out.println("This should never happen");
+			}
+		}
 }
---
+++ src/Sorter.java
@@ -0,0 +1,168 @@
+
+public class Sorter {
+	
+	private int[] tempMergArr;
+	private int N;
+	private int[] array;
+	private int length;
+	
+	
+//Insertion
+public int[] insertion(int[] input){
+    
+    int temp;
+    for (int i = 1; i < input.length; i++) {
+        for(int j = i ; j > 0 ; j--){
+            if(input[j] < input[j-1]){
+                temp = input[j];
+                input[j] = input[j-1];
+                input[j-1] = temp;
+            }
+        }
+    }
+    return input;
+}
+	
+//Merge
+public void startMerge(int inputArr[]) {
+    array = inputArr;
+    length = array.length;
+    int[] tempMergArr = new int[length];
+    mergeSort(0, length - 1);
+}
+
+public void mergeSort(int lowerIndex, int higherIndex) {
+    
+   if (lowerIndex < higherIndex) {
+       int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
+       // Below step sorts the left side of the array
+       mergeSort(lowerIndex, middle);
+       // Below step sorts the right side of the array
+       mergeSort(middle + 1, higherIndex);
+       // Now merge both sides
+       mergeParts(lowerIndex, middle, higherIndex);
+   }
+}
+
+public void mergeParts(int lowerIndex, int middle, int higherIndex) {
+	 
+    for (int i = lowerIndex; i <= higherIndex; i++) {
+        tempMergArr[i] = array[i];
+    }
+    int i = lowerIndex;
+    int j = middle + 1;
+    int k = lowerIndex;
+    while (i <= middle && j <= higherIndex) {
+        if (tempMergArr[i] <= tempMergArr[j]) {
+            array[k] = tempMergArr[i];
+            i++;
+        } else {
+            array[k] = tempMergArr[j];
+            j++;
+        }
+        k++;
+    }
+    while (i <= middle) {
+        array[k] = tempMergArr[i];
+        k++;
+        i++;
+    }
+
+}
+
+//Bubble
+public int[] bubbleSort(int[] numArray) {
+	array = numArray;
+	length = array.length;
+	int temp = 0;
+	for (int i = 0; i < length; i++) {
+		for (int j = 1; j < (length - i); j++) {
+			if (array[j - 1] < array[j]) {
+				temp = array[j - 1];
+				array[j - 1] = array[j];
+				array[j] = temp;
+			}
+		}
+	}
+	return array;
+}
+
+//Quick
+public void qSort(int[] values)
+{
+	array = values;
+	length = array.length;
+	quickSort(0, length - 1);
+}
+
+public void quickSort(int lowerIndex, int higherIndex) {
+	int i = lowerIndex;
+	int j = higherIndex;
+	int pivot = array[lowerIndex + (higherIndex - lowerIndex) / 2];
+	while (i <= j) {
+		while (array[i] < pivot) {
+			i++;
+		}
+		while (array[j] > pivot) {
+			j--;
+		}
+		if (i <= j) {
+			exchangeNumbers(i , j);
+			i++;
+			j--;
+		}
+	}
+	if (lowerIndex < j) {
+		quickSort(lowerIndex, j);
+	}
+	if (i < higherIndex) {
+		quickSort(i, higherIndex);
+	}
+}
+
+public void exchangeNumbers(int i, int j) {
+	int temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
+}
+
+//Heap
+public void heapSort(int array[]) {
+	heapify(array);
+	for (int i = N; i > 0; i--) {
+		swap(array, 0, i);
+		N = N - 1;
+		maxHeap(array, 0);
+	}
+}
+
+public void heapify(int array[]) {
+	N = array.length - 1;
+	for (int i = N / 2; i >= 0; i--) {
+		maxHeap(array, i);
+	}
+}
+
+public void maxHeap(int array[], int i) {
+	int left = 2 * i;
+	int right = 2 * i + 1;
+	int max = i;
+	if (left <= N && array[left] > array[i]) {
+		max = left;
+	}
+	if (right <= N && array[right] > array[max]) {
+		max = right;
+	}
+	if (max != i) {
+		swap(array, i, max);
+		maxHeap(array, max);
+	}
+}
+
+public void swap(int array[], int i, int j) {
+	int temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
+}
+
+}
